8594ff2060f94bea43b3c770681952588d322568
arunsub94 <arunlp94@gmail.com>
2020-08-09 10:03:37 -0400

    Revisions to problem_4.py and explanation_4.md

--- explanation_4.md
+++ explanation_4.md
@@ -1,5 +1,7 @@
 Explanation for Problem 4
 
+This logic was discussed in the Udacity course problem Sort-0-1-2. This problem demands the exact same approach. Any similarities are owing to the fact that these two problems are one and the same. 
+
 We know that the 0s lump together at the start and all the 2s can be found at the end.
 Since the constraint is one of single traversal, we employ pointers to keep tabs on the next 0 position and
 the next 2 position and since we know that it's either 0, 1 or 2 in the array, the only other option left is 1.
--- problem_4.py
+++ problem_4.py
@@ -1,42 +1,35 @@
-def sort_012(input_list):
-    """
-    Given an input array consisting on only 0, 1, and 2, sort the array in a single traversal.
+def sort_dutch(arr):
 
-    Args:
-       input_list(list): List to be sorted
-    """
+    #Two pointers for the next 0 position and 2 position
+    pointer_0 = 0
+    pointer_2 = len(arr) - 1
 
-    next_pos_0 = 0
-    next_pos_2 = len(input_list) - 1
-
-    front_index = 0
-
-    while front_index <= next_pos_2:
-        if input_list[front_index] == 0:
-            input_list[front_index] = input_list[next_pos_0]
-            input_list[next_pos_0] = 0
-            next_pos_0 += 1
-            front_index += 1
-        elif input_list[front_index] == 2:
-            input_list[front_index] = input_list[next_pos_2]
-            input_list[next_pos_2] = 2
-            next_pos_2 -= 1
+    index = 0
+    while index <= pointer_2:
+        if(arr[index] == 0):
+            arr[index] = arr[pointer_0]
+            arr[pointer_0] = 0
+            pointer_0 += 1
+            index += 1
+        elif(arr[index] == 2):
+            arr[index] = arr[pointer_2]
+            arr[pointer_2] = 2
+            pointer_2 -= 1
         else:
-            front_index += 1
+            arr[index] = 1
+            index += 1
 
-    return input_list
+    return arr
 
 def test_function(test_case):
-    sorted_array = sort_012(test_case)
-    print(sorted_array)
-    if sorted_array == sorted(test_case):
+    dutch_array = sort_dutch(test_case)
+    print(dutch_array)
+    if dutch_array == sorted(test_case):
         print("Pass")
     else:
         print("Fail")
 
-test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2]) # should return [0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 2]
-test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
-test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+test_function([0,2,1,1,2,0,0])
 test_function([1,0])
 test_function([2,1])
 test_function([1,1,1])
